bf787a450aaad1520df60ca220c8e397d8c9773c
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-23 05:03:56 +0800

    Use custom battery icon

--- src/app/components/PhoneDetails.js
+++ src/app/components/PhoneDetails.js
@@ -13,6 +13,7 @@ import {
   WifiLow,
   WifiZero,
 } from "lucide-react";
+import React, { useState } from "react";
 
 export default function PhoneDetails({ phoneSettings }) {
   return (
@@ -45,21 +46,39 @@ export default function PhoneDetails({ phoneSettings }) {
         </span>
         <span className="flex items-end text-sm">
           {phoneSettings.battery}%
-          <span className="ml-1 flex items-end">
-            {phoneSettings.battery >= 75 ? (
-              <BatteryFull />
-            ) : phoneSettings.battery >= 50 ? (
-              <BatteryMedium />
-            ) : phoneSettings.battery >= 25 ? (
-              <BatteryLow />
-            ) : phoneSettings.battery > 5 ? (
-              <Battery />
-            ) : (
-              <BatteryWarning />
-            )}
-          </span>
+          <BatteryIcon batteryLevel={phoneSettings.battery} />
         </span>
       </div>
     </div>
+  );
+}
+
+function BatteryIcon({ batteryLevel }) {
+  const getBatteryColor = (level) => {
+    if (level <= 20) return "#ff3b30";
+    if (level <= 50) return "#ffcc00";
+    return "#34c759";
+  };
+
+  return (
+    <svg
+      width="40"
+      height="28"
+      viewBox="0 0 60 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-5 -rotate-90"
+    >
+      <rect x="1" y="1" width="35" height="28" rx="3" fill="gray" />
+      <rect x="36" y="8" width="5" height="12" fill="gray" />
+      <rect
+        x="3"
+        y="3"
+        width={Math.min(31 * (batteryLevel / 100), 31)}
+        height="24"
+        rx="2"
+        fill={getBatteryColor(batteryLevel)}
+      />
+    </svg>
   );
 }
